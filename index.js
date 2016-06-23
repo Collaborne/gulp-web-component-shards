e076a0266727358074386e335acb268bb99bee07
Andreas Kohn <andreas.kohn@gmail.com>
2016-06-23 18:20:06 +0200

    Switch from Node's core 'stream' to 'through2'
    This just makes it ever so slightly easier to read, but it doesn't
    help getting the error out properly yet.
    
    While there: add some debug output, which will go away at the end
    again.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 var WebComponentShards = require('web-component-shards');
 var path = require('path');
 var gulp = require('gulp');
-var stream = require('stream');
+var through2 = require('through2');
 var util = require('util');
+var gutil = require('gulp-util');
 
 /**
  * @typedef Config
@@ -15,38 +16,6 @@ var util = require('util');
  * @property {string} threshold
  * @property {string} [depReport] file name for a dependency report (JSON)
  */
-
-function WebComponentShardsStream(options) {
-
-	options.endpoints = [];
-
-	this._transform = function(file, enc, cb) {
-		try {
-			options.endpoints.push(file.relative);
-			cb();
-		} catch (err) {
-			cb(err);
-		}
-	}.bind(this);
-	this._flush = function(cb) {
-		var stream = this;
-
-		// Run the actual sharding tool
-		var shards = new WebComponentShards(options);
-		shards.build().then(function() {
-			// Now collect all files from options.dest_dir for further processing.
-			gulp.src(options.dest_dir + '/**/*.html').on('data', function(file) {
-				stream.push(file);
-			}).on('end', function() {
-				cb();
-			});
-		});
-	}.bind(this);
-
-	stream.Transform.call(this, {objectMode: true});
-}
-
-util.inherits(WebComponentShardsStream, stream.Transform);
 
 /**
  * @param {Config} [opts] the configuration
@@ -62,12 +31,57 @@ function gwcs(opts) {
 		sharing_threshold: config.threshold || 2,
 		dest_dir: path.resolve(work, 'dist'),
 		workdir: path.resolve(work, 'tmp'),
-		depReport: config.depReport ? path.resolve(process.cwd(), config.depReport) : undefined
+		depReport: config.depReport ? path.resolve(process.cwd(), config.depReport) : undefined,
+		endpoints: []
 	};
+
 	// Collect all input files into endpoints, run the tool, and return new vinyl instances for the produced output.
 	// XXX: for now this doesn't handle situations where the contents of the files matters, rather we assume here
 	//		that these files have not been touched.
-	return new WebComponentShardsStream(wcsOptions);
+
+	function discoverEndpoint(file, enc, cb) {
+		// Note that we're only calling cb() once at the end, and use _push/_emit inside the try-catch.
+		// This avoids situations where we get another error in the callback, trigger the catch, and then
+		// call the callback again.
+		// See https://github.com/sindresorhus/gulp-esnext/issues/8 for a similar example.
+		var discoverErr;
+		try {
+			console.log('Considering ' + file.relative + ' as endpoint');
+			wcsOptions.endpoints.push(file.relative);
+		} catch (err) {
+			console.log('Error in collecting endpoints: ' + err);
+			discoverErr = err;
+		}
+
+		if (discoverErr) {
+			cb(new gutil.PluginError('gulp-web-component-shards', discoverErr));
+		} else {
+			cb(null, file);
+		}
+	}
+
+	function processAndCollect() {
+		var emit = this.emit.bind(this);
+
+		// Run the actual sharding tool
+		var shards = new WebComponentShards(wcsOptions);
+		return shards.build().then(function() {
+			// Now collect all files from options.dest_dir for further processing.
+			console.log('Collecting output from ' + options.dest_dir);
+			gulp.src(options.dest_dir + '/**/*.html').on('data', function(file) {
+				console.log('Found output: '+ file.relative);
+				emit('data', file);
+			}).on('end', function() {
+				console.log('Found all output, calling cb');
+				emit('end');
+			});
+		}).catch(function(err) {
+			console.log('Error in build(): ' + err);
+			emit('error', new gutil.PluginError('gulp-web-component-shards', err));
+		});
+	}
+
+	return through2.obj(discoverEndpoint, processAndCollect);
 };
 
 module.exports = gwcs;
